10294598cb00d5ab180bae2196464ea9600e27ea
Wing <genxium@hotmail.com>
2018-02-03 01:20:12 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,28 @@
+Project root
+==
+
+You're reading `<proj-root>/README.md`.
+
+Building
+==
+
+user@proj-root> gradle build [-b build.gradle] --info
+
+Deployment
+==
+
+Assuming that a preconfigured Tomcat@8.0.24 container is used, and that a 
+
+```
+<Connector port="8080" protocol="HTTP/1.1"
+               connectionTimeout="20000" />
+```
+
+is defined in `${catalina.base}/conf/server.xml` where ["HTTP/1.1" specifies WebSocket support by RFC6455](https://tools.ietf.org/html/rfc6455).
+
+```
+user@proj-root> scp ./build/libs/ws-servlet-sample.war /path/to/catalina/base/webapps/
+user@${catalina.base}> sh bin/catalina.sh [run | start]
+```
+
+Visit `http://${whatever-host-configured}:8080/ws-servlet-sample/echo.html`.
---
+++ src/main/java/ws/sample/endpoint/Echo.java
@@ -0,0 +1,50 @@
+package ws.sample.endpoint;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ServerEndpoint("/echo")
+public class Echo {
+  
+  private static final Logger logger = LoggerFactory.getLogger(Echo.class);
+  
+  @OnOpen
+  public void onOpen(Session session) {
+    logger.info("{} has opened a connection.", session.getId());
+    try {
+      session.getBasicRemote().sendText("Connection Established");
+    } catch (IOException ex) {
+      logger.error(ex.getMessage(), ex);
+    }
+  }
+  
+  @OnClose
+  public void onClose(Session session) {
+    logger.warn("Session {} has ended.", session.getId());
+  }
+  
+  @OnMessage
+  public void onMessage(String message, Session session) {
+    logger.info("Message from {}: {}", session.getId() , message);
+    try {
+      session.getBasicRemote().sendText(message);
+    } catch (IOException ex) {
+      logger.error(ex.getMessage(), ex);
+    }
+  }
+  
+  @OnError
+  public void onError(Throwable e) {
+    logger.error(e.getMessage(), e);
+  }
+  
+}

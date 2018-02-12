dee9391e5e97b05f5852f322f63bcf719baa268c
Wing <genxium@hotmail.com>
2018-02-12 20:59:17 +0800

    Minor update.

--- src/main/java/ws/sample/Boot.java
+++ src/main/java/ws/sample/Boot.java
@@ -113,7 +113,7 @@ public class Boot implements javax.servlet.ServletContextListener {
     }
   }
   
-  private DataSource getMysqlConnDs() {
+  public DataSource getMysqlConnDs() {
     return mysqlConnDs;
   }
 }
